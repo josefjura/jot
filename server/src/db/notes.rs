d5f7932b29b3ecfe30a21043388cb1675d7970bd
Jura, Josef <josef.jura@hotmail.com>
2025-11-16 10:41:57 +0100

    Removed unused function

--- server/src/db/notes.rs
+++ server/src/db/notes.rs
@@ -6,21 +6,6 @@ use crate::{
     router::note::NoteSearchRequest,
 };
 
-pub async fn get_all(db: SqlitePool) -> Result<Vec<Note>, DbError> {
-    let items = sqlx::query_as!(NoteEntity, "SELECT * FROM notes")
-        .fetch_all(&db)
-        .await
-        .map_err(|e| {
-            tracing::error!("Failed to get all repositories: {:?}", e);
-            DbError::Unknown(e)
-        })?;
-
-    items
-        .into_iter()
-        .map(|item| item.try_into().map_err(DbError::EntityMapping))
-        .collect()
-}
-
 pub async fn get_all_by_user(db: SqlitePool, id: i64) -> Result<Vec<Note>, DbError> {
     let items = sqlx::query_as!(
         NoteEntity,
