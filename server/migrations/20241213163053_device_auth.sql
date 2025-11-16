72998b98e17c84fa2e6cca8211456cc5bfdc9d0c
Jura, Josef <josef.jura@hotmail.com>
2025-11-16 10:42:06 +0100

    Fixed SQL migration

--- server/migrations/20241213163053_device_auth.sql
+++ server/migrations/20241213163053_device_auth.sql
@@ -1,7 +1,7 @@
 -- Table that can hold data for auth attempts (id, expire_date, device_code, token)
 CREATE TABLE device_auth (
-    id SERIAL PRIMARY KEY,
-    expire_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    device_code VARCHAR(255) NOT NULL,
-    token VARCHAR(255) NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    expire_date TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    device_code TEXT NOT NULL,
+    token TEXT NULL
 );
